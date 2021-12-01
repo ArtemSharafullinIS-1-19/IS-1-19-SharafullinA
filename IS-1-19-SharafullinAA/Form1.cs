d2bbbf5d3e2be9182310025a9dd0693f9233c41c
Артём шарафуллин <rejs03@mail.ru>
2021-12-01 22:02:49 +0500

    Задание 1

--- IS-1-19-SharafullinAA/Form1.cs
+++ IS-1-19-SharafullinAA/Form1.cs
@@ -19,59 +19,133 @@ namespace IS_1_19_SharafullinAA
 
         private void Form1_Load(object sender, EventArgs e)
         { }
-            abstract class Detalis
+        abstract class Komplektaciya<art>
         {
-            public int stoimost;
-            public int god;
-            public abstract void Info(ListBox l);
-            public Detalis(int stoimostt, int godd)
+            protected int stoimost;
+            protected string god;
+            protected art artikul;
+            public Komplektaciya(int stoimostt, string godd, art artikull)
             {
                 stoimost = stoimostt;
                 god = godd;
+                artikul = artikull;
             }
+            public abstract void Display(ListBox lb);
         }
-        class CP : Detalis
+        class CP : Komplektaciya<string>
         {
-            public int yadra;
-            public int potoki;
-            public int chastota;
-            public override void Info(ListBox l)
+            double chastota;
+            int yadro;
+            int potoki;
+            public CP(int stoimostt, string godd, string artikull, double chastotaa, int yadroo, int potokii)
+                : base(stoimostt, godd, artikull)
             {
-                l.Items.Add($"Стоимость - {stoimost}, Год выпуска - {god}, Колличество ядер - {yadra}, Колличество потоков - {potoki}, Частота процессора - {chastota}");
-            }
-            public CP(int yadraa, int potokii, int chastotaa, int stoimost, int god) : base(god, stoimost)
-            {
-                yadra = yadraa;
-                potoki = potokii;
                 chastota = chastotaa;
+                yadro = yadroo;
+                potoki = potokii;
+            }
+            public double chastotaa { get { return chastotaa; } set { chastotaa = value; } }
+            public int yadroo { get { return yadroo; } set { yadroo = value; } }
+            public int potokii { get { return potokii; } set { potokii = value; } }
+
+            public override void Display(ListBox listBox1)
+            {
+                listBox1.Items.Add($"Цена - {stoimost}, Год выпуска - {god}, Арктикул - {artikul}, частота - {chastota}, Число ядер - {yadro}, Число потоков - {potoki}");
             }
         }
-        class Videocarta : Detalis
+        class Videokartachka : Komplektaciya<string>
         {
-            public int chastota_GPU;
-            public int moshnost;
-            public int pamyat;
-            public override void Info(ListBox l)
+            double chastota;
+            string firma;
+            int pamyat;
+            public Videokartachka(int stoimostt, string godd, string artikull, double chastotaa, string firmaa, int pamyatt)
+                : base(stoimostt, godd, artikull)
             {
-                l.Items.Add($"Частота видеокарты - {chastota_GPU}, Производительность - {moshnost}, Обьем памяти - {pamyat}, Стоимость - {stoimost}, Год выпуска - {god}");
-            }
-            public Videocarta(int chastotaGPUU, int moshnostt, int pamyatt, int stoimost, int god) : base(god, stoimost)
-            {
-                chastota_GPU = chastotaGPUU;
-                moshnost = moshnostt;
+                chastota = chastotaa;
+                firma = firmaa;
                 pamyat = pamyatt;
             }
-        }
+             public double chastotaa { get { return chastotaa; } set { chastotaa = value; } }
+            public string firmaa { get { return firmaa; } set { firmaa = value; } }
+            public int pamyatt { get { return pamyatt; } set { pamyatt = value; } }
 
-        private void button1_Click(object sender, EventArgs e) 
+            public override void Display(ListBox listBox1)
+            {
+             listBox1.Items.Add($"Цена: {stoimost}, год Выпуска: {god}, арктикул: {artikul}, частота GPU: {chastota}, производитель: {firma}, объём Памяти {pamyat}");
+
+            }
+        }
+        CP cp;
+        Videokartachka GPU;
+
+        void button1_Click(object sender, EventArgs e)
         {
-            CP sot = new CP(6, 6, 29, 2018, 11000);
-            sot.Info(listBox1);
+           int stoimostt = Convert.ToInt32(textBox1.Text);
+           string godd = textBox2.Text;
+           double chastotaa = Convert.ToDouble(textBox3.Text);
+           int yadroo = Convert.ToInt32(textBox4.Text);
+           int potokii = Convert.ToInt32(textBox5.Text);
+           string artikull = textBox9.Text;
+           cp = new CP(stoimostt, godd, artikull, chastotaa, yadroo, potokii);
+           cp.Display(listBox1);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Videocarta sot = new Videocarta(16, 23000, 6, 2018, 20000);
-            sot.Info(listBox1);
+             int stoimostt = Convert.ToInt32(textBox1.Text);
+             string godd = textBox2.Text;
+            double chastotaa = Convert.ToDouble(textBox6.Text);
+             string firmaa = textBox7.Text;
+             int pamyatt = Convert.ToInt32(textBox8.Text);
+            string artikull = textBox9.Text;
+             GPU = new Videokartachka(stoimostt, godd, artikull, chastotaa, firmaa, pamyatt);
+             GPU.Display(listBox1);
+        }
+
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Цена")
+                textBox1.Text = "";
+        }
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Год Выпуска")
+                textBox2.Text = "";
+        }
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "Частота")
+                textBox3.Text = "";
+        }
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == "Количество ядер")
+                textBox4.Text = "";
+        }
+        private void textBox5_Click(object sender, EventArgs e)
+        {
+            if (textBox5.Text == "Количество потоков")
+                textBox5.Text = "";
+        }
+        private void textBox6_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text == "Частота GPU")
+                textBox6.Text = "";
+        }
+        private void textBox7_Click(object sender, EventArgs e)
+        {
+            if (textBox7.Text == "Производитель")
+                textBox7.Text = "";
+        }
+        private void textBox8_Click(object sender, EventArgs e)
+        {
+            if (textBox8.Text == "Объём Памяти")
+                textBox8.Text = "";
+        }
+        private void textBox9_Click(object sender, EventArgs e)
+        {
+            if (textBox9.Text == "Артикул")
+                textBox9.Text = "";
         }
 }   } 
